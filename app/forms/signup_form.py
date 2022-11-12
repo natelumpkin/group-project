51da989d45fb1953c9ea58d6e4276b6424447faf
Bradley Lewter <rakugo1@gmail.com>
2022-11-11 19:40:58 -0500

    Media and Signup form updated

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -23,5 +23,8 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
+    first_name = StringField("first name", validators=[DataRequired()])
+    last_name = StringField("last name", validators=[DataRequired()])
+    profile_image_url = StringField("profile image url", validators=[URL(), OPTIONAL()])
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
