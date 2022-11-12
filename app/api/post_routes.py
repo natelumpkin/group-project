7fa5618a462e06528ffa20d6c5d23e4f282227d3
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-11-11 20:01:12 -0800

    get all posts in progress

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -3,3 +3,17 @@ from flask_login import current_user
 from app.models import Post, Comment, User, Media
 
 post_routes = Blueprint('posts', __name__)
+
+@post_routes.route('/')
+def get_all_posts():
+  posts = Post.query.order_by(Post.created_at).all()
+  response = {
+    "Posts": []
+  }
+  for post in posts:
+    post_dict = post.to_dict()
+    post_dict['Media'] = []
+    post_dict['User'] = {}
+    post_author = post.author
+    print(post_author)
+  return 'Hello from get all posts'
