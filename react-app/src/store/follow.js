5c7fe7c41ed740c04aae5adced072dc0ef79f1e7
David Siwulec <david.siwulec@gmail.com>
2022-11-13 15:21:34 -0500

    Completed follow reducer skeleton

--- react-app/src/store/follow.js
+++ react-app/src/store/follow.js
@@ -1,3 +1,21 @@
+const CREATE_FOLLOW = 'follow/CREATE_FOLLOW'
+const LOAD_FOLLOWS = 'follow/LOAD_FOLLOWS'
+const DELETE_FOLLOW = 'follow/DELETE_FOLLOW'
+
+const addFollow = () => ({
+  type: CREATE_FOLLOW
+})
+
+const loadFollows = () => ({
+  type: LOAD_FOLLOWS
+})
+
+const removeFollow = () => ({
+  type: DELETE_FOLLOW
+})
+
+initialState = { followers: {}, following: {} }
+
 export default followReducer = (state = initialState, action) => {
   return state
 }
