12ea9779f2fb69ef58aa164e3af014cec838bea7
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-11-13 10:16:52 -0800

    removed prints, added schema to follow and like table

--- app/models/follow.py
+++ app/models/follow.py
@@ -1,10 +1,14 @@
-from .db import db, SCHEMA, add_prefix_for_prod
+from .db import db, SCHEMA, add_prefix_for_prod, environment
 from datetime import datetime
 
 follows = db.Table(
-  add_prefix_for_prod('follows'),
+  'follows',
   db.Model.metadata,
   db.Column('follower_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'))),
   db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'))),
-  db.Column('created_at', db.DateTime, default=datetime.utcnow())
+  db.Column('created_at', db.DateTime, default=datetime.utcnow()),
+  schema=SCHEMA if environment == "production" else None
 )
+
+if environment == "production":
+  follows.schema = SCHEMA
