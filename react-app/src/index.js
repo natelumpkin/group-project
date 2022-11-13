ba9207733dc3d085c7c22113b8c2557e931143f8
Max Yaswen <myaswen@gmail.com>
2022-11-13 14:50:55 -0800

    Add like reducer & get post likes thunk

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import * as postActions from './store/post'
+import * as likeActions from './store/like';
 
 const store = configureStore();
 
 if (process.env !== 'production') {
   window.store = store;
   window.postActions = postActions;
+  window.likeActions = likeActions;
 }
 
 ReactDOM.render(
--- react-app/src/store/like.js
+++ react-app/src/store/like.js
@@ -1,7 +1,48 @@
-const initialState = {}
+import normalizeData from "../utils/normalize";
 
+// Action types:
+const LOAD_POST_LIKES = '/post/likes/all';
+
+
+// Action creators:
+const loadPostLikes = (postId, postLikes) => ({
+  type: LOAD_POST_LIKES,
+  payload: {
+    postId,
+    postLikes
+  }
+})
+
+
+// Thunk action creators:
+export const getPostLikes = (postId) => async (dispatch) => {
+  const response = await fetch(`/api/posts/${postId}/likes`);
+
+  if (response.ok) {
+    const postLikes = await response.json();
+    dispatch(loadPostLikes(postId, postLikes))
+  } else {
+    return [`Unable to fetch post ${postId}'s likes.`]
+  }
+}
+
+
+// Initial 'likes' state:
+const initialState = {
+  posts: {}
+}
+
+// 'likes' reducer:
 const likeReducer = (state = initialState, action) => {
-  return state
+  let newState;
+  switch (action.type) {
+    case LOAD_POST_LIKES:
+      newState = {...state};
+      newState.posts[action.payload.postId] = normalizeData(action.payload.postLikes.Likes);
+      return newState;
+    default:
+      return state;
+  }
 }
 
 export default likeReducer;
