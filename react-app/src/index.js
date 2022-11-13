cad8285a7016f8e7e9a39c01fad790d5865b8872
Bradley Lewter <rakugo1@gmail.com>
2022-11-13 16:51:42 -0500

    Adding commentActions to Index

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import * as postActions from './store/post'
+import * as commentActions from './store/comment'
 
 const store = configureStore();
 
 if (process.env !== 'production') {
   window.store = store;
   window.postActions = postActions;
+  window.commentActions = commentActions;
 }
 
 ReactDOM.render(
