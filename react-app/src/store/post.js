7c510689d0d5c33ca3dad5f5b4107966771700c3
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-11-13 21:26:20 -0800

    posts reducer nearing completion

--- react-app/src/store/post.js
+++ react-app/src/store/post.js
@@ -8,7 +8,7 @@ const EDIT_POST = '/posts/edit'
 const DELETE_POST = '/posts/delete'
 const ADD_MEDIA = '/posts/add_media'
 
-// Thunks
+// Action Creators
 
 const getPosts = (posts) => ({
   type: ALL_POSTS,
@@ -28,6 +28,21 @@ const getUserPosts = (posts) => ({
 const addPost = (post) => ({
   type: ADD_POST,
   payload: post
+})
+
+const editPost = (editedPost) => ({
+  type: EDIT_POST,
+  payload: editedPost
+})
+
+const removePost = (postId) => ({
+  type: DELETE_POST,
+  payload: postId
+})
+
+const addMedia = (media) => ({
+  type: ADD_MEDIA,
+  payload: media
 })
 
 // Thunk Action Creators
@@ -81,8 +96,15 @@ export const createPost = (postData) => async (dispatch) => {
     body: JSON.stringify(postData),
     headers: {"Content-Type": "application/json"}
   });
+  const sessionResponse = await fetch('/api/auth/', {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
   if (response.ok) {
     const newPost = await response.json()
+    const userData = await sessionResponse.json()
+    newPost.User = userData
     dispatch(addPost(newPost))
     return newPost
   } else {
@@ -97,11 +119,58 @@ const demoPost = {
   text: "In 24 short weeks"
 }
 
+export const updatePost = (postId, postBody) => async dispatch => {
+  const response = await fetch(`/api/posts/${postId}`, {
+    method: "PUT",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(postBody)
+  })
+  if (response.ok) {
+    const newPost = await response.json();
+    dispatch(editPost(newPost));
+    return newPost;
+  } else {
+    const errors = await response.json();
+    return errors;
+  }
+}
+
+export const deletePost = (postId) => async (dispatch) => {
+  const response = await fetch(`/api/posts/${postId}`, {
+    method: "DELETE"
+  });
+
+  if (response.ok) {
+    const successMessage = await response.json();
+    dispatch(removePost(postId));
+    return successMessage;
+  } else {
+    const errors = await response.json();
+    return errors;
+  }
+}
+
+export const addMediaByPostId = (postId, media) => async dispatch => {
+  const response = await fetch(`/api/posts/${postId}/media`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify(media)
+  })
+  if (response.ok) {
+    const newMedia = await response.json()
+    dispatch(addMedia(newMedia))
+    return newMedia
+  } else {
+    const errors = await response.json()
+    return errors;
+  }
+}
+
 // Initial State
 
 const initialState = {
-  allPosts: [],
-  userPosts: []
+  allPosts: {},
+  userPosts: {}
 }
 
 // Reducer
@@ -115,15 +184,16 @@ const postReducer = (state = initialState, action) => {
         userPosts: {}
       }
       for (let postId in state.userPosts) {
-        console.log(postId)
         newState.userPosts[postId] = {
           ...state.userPosts[postId],
           User: {
             ...state.userPosts[postId].User
-          },
-          Media: [
-            ...state.userPosts[postId].Media
-          ]
+          }
+        }
+        const mediaData = state.userPosts[postId].Media
+        newState.userPosts[postId].Media = []
+        for (let media of mediaData) {
+          newState.userPosts[postId].Media.push({...media})
         }
       }
 
@@ -136,15 +206,16 @@ const postReducer = (state = initialState, action) => {
           userPosts: {}
         }
         for (let postId in state.userPosts) {
-          console.log(postId)
           newState.userPosts[postId] = {
             ...state.userPosts[postId],
             User: {
               ...state.userPosts[postId].User
-            },
-            Media: [
-              ...state.userPosts[postId].Media
-            ]
+            }
+          }
+          const mediaData = state.userPosts[postId].Media
+          newState.userPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.userPosts[postId].Media.push({...media})
           }
         }
         return newState;
@@ -156,52 +227,201 @@ const postReducer = (state = initialState, action) => {
           userPosts: data
         }
         for (let postId in state.allPosts) {
-          console.log(postId)
           newState.allPosts[postId] = {
             ...state.allPosts[postId],
             User: {
               ...state.allPosts[postId].User
-            },
-            Media: [
-              ...state.allPosts[postId].Media
-            ]
+            }
+          }
+          const mediaData = state.allPosts[postId].Media
+          newState.allPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.allPosts[postId].Media.push({...media})
           }
         }
         return newState;
       }
       case ADD_POST: {
         const post = action.payload
-        console.log(post)
+        // console.log(post)
         const newState = {
           allPosts: {},
           userPosts: {}
         }
         for (let postId in state.allPosts) {
-          console.log(postId)
           newState.allPosts[postId] = {
             ...state.allPosts[postId],
             User: {
               ...state.allPosts[postId].User
-            },
-            Media: [
-              ...state.allPosts[postId].Media
-            ]
+            }
+          }
+          const mediaData = state.allPosts[postId].Media
+          newState.allPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.allPosts[postId].Media.push({...media})
           }
         }
         for (let postId in state.userPosts) {
-          console.log(postId)
           newState.userPosts[postId] = {
             ...state.userPosts[postId],
             User: {
               ...state.userPosts[postId].User
-            },
-            Media: [
-              ...state.userPosts[postId].Media
-            ]
+            }
+          }
+          const mediaData = state.userPosts[postId].Media
+          newState.userPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.userPosts[postId].Media.push({...media})
           }
         }
-        newState.allPosts[post.id] = post
+
+        newState.allPosts[post.id] = {
+          ...post,
+          Media: [],
+          User: {
+            following: false,
+            id: post.User.id,
+            profileImageUrl: post.User.profileImageUrl,
+            username: post.User.username
+          }
+        }
+
         return newState
+      }
+      case EDIT_POST: {
+        const editedPost = action.payload
+        const newState = {
+          allPosts: {},
+          userPosts: {}
+        }
+        for (let postId in state.allPosts) {
+          newState.allPosts[postId] = {
+            ...state.allPosts[postId],
+            User: {
+              ...state.allPosts[postId].User
+            }
+          }
+          const mediaData = state.allPosts[postId].Media
+          newState.allPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.allPosts[postId].Media.push({...media})
+          }
+        }
+        for (let postId in state.userPosts) {
+          newState.userPosts[postId] = {
+            ...state.userPosts[postId],
+            User: {
+              ...state.userPosts[postId].User
+            }
+          }
+          const mediaData = state.userPosts[postId].Media
+          newState.userPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.userPosts[postId].Media.push({...media})
+          }
+        }
+        // if post existed on user posts
+        if (state.userPosts[editedPost.id]) {
+          // Get previous post's userdata and media data
+          // spread it into edited post
+          // console.log('mediaData: ', mediaData);
+          newState.userPosts[editedPost.id] = editedPost
+          const mediaData = state.userPosts[editedPost.id].Media
+          newState.userPosts[editedPost.id].Media = []
+          for (let media of mediaData) {
+            newState.userPosts[editedPost.id].Media.push({...media})
+          }
+          const userData = state.userPosts[editedPost.id].User
+          newState.userPosts[editedPost.id].User = { ...userData }
+        }
+        if (state.allPosts[editedPost.id]) {
+          newState.allPosts[editedPost.id] = editedPost
+          const mediaData = state.allPosts[editedPost.id].Media
+          newState.allPosts[editedPost.id].Media = []
+          for (let media of mediaData) {
+            newState.allPosts[editedPost.id].Media.push({...media})
+          }
+          const userData = state.allPosts[editedPost.id].User
+          newState.allPosts[editedPost.id].User = { ...userData }
+        }
+        return newState;
+      }
+      case DELETE_POST: {
+        const postId = action.payload
+        const newState = {
+          allPosts: {},
+          userPosts: {}
+        }
+        for (let postId in state.allPosts) {
+          newState.allPosts[postId] = {
+            ...state.allPosts[postId],
+            User: {
+              ...state.allPosts[postId].User
+            }
+          }
+          const mediaData = state.allPosts[postId].Media
+          newState.allPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.allPosts[postId].Media.push({...media})
+          }
+        }
+        for (let postId in state.userPosts) {
+          newState.userPosts[postId] = {
+            ...state.userPosts[postId],
+            User: {
+              ...state.userPosts[postId].User
+            }
+          }
+          const mediaData = state.userPosts[postId].Media
+          newState.userPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.userPosts[postId].Media.push({...media})
+          }
+        }
+        delete newState.userPosts[postId];
+        delete newState.allPosts[postId];
+        return newState;
+      }
+      case ADD_MEDIA: {
+        // spread all the data from the previous states
+        const media = action.payload;
+        const postId = media.postId
+        const newState = {
+          allPosts: {},
+          userPosts: {}
+        }
+        for (let postId in state.allPosts) {
+          newState.allPosts[postId] = {
+            ...state.allPosts[postId],
+            User: {
+              ...state.allPosts[postId].User
+            }
+          }
+          const mediaData = state.allPosts[postId].Media
+          newState.allPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.allPosts[postId].Media.push({...media})
+          }
+        }
+        for (let postId in state.userPosts) {
+          newState.userPosts[postId] = {
+            ...state.userPosts[postId],
+            User: {
+              ...state.userPosts[postId].User
+            }
+          }
+          const mediaData = state.userPosts[postId].Media
+          newState.userPosts[postId].Media = []
+          for (let media of mediaData) {
+            newState.userPosts[postId].Media.push({...media})
+          }
+        }
+        newState.allPosts[postId].Media.push(media)
+        newState.userPosts[postId].Media.push(media)
+        // if the post is in either state, add the media to
+        // that post's Media array
+        // return new state
+        return newState;
       }
     default:
       return state;
