22f73730c69638888f1acc577c233f927d3ad795
Bradley Lewter <rakugo1@gmail.com>
2022-11-15 13:41:08 -0500

    PostCommentCard nearly there.

---
+++ react-app/src/components/PostCommentCard/CommentInput.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, useHistory, NavLink, Link } from "react-router-dom";
+import './PostCommentCard.css';
+import * as commentActions from "../../store/comment";
+
+
+const CommentInput = ({ postId }) => {
+  const dispatch = useDispatch()
+  const sessionUser = useSelector(state => state.session.user);
+  const [comment, setComment] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+      return dispatch(commentActions.createPostComment({ comment, postId, sessionUser }))
+    // .then(()=>{
+    //   What to do...
+    // })
+      .catch(async (res) => {
+        const data = await res.json();
+
+        if (data && data.errors) setErrors(data.errors);
+      });
+  }
+
+return (
+    <div>
+      <div className="post-comment-main-container">
+        <div className="post-comment-user-icon">
+          {sessionUser ? sessionUser.profileImageUrl : "👨‍👧‍👧" }
+        </div>
+        <div className="post-comment-input-container">
+          <form onSubmit={handleSubmit} className="post-comment-new">
+          <div className="post-comment-input-field">
+            <input
+              type="test"
+              value={comment}
+              onChange={(e)=>setComment(e.target.value)}
+              required
+              className="post-comment-input-bar"
+              placeholder="Unleash Compliments!"
+            />
+
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
+)
+}
+
+export default CommentInput;
