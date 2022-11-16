835c2bb4a55b3e15c5474598cd3a24772b805b50
Nate Lumpkin <nate.lumpkin@gmail.com>
2022-11-16 10:49:31 -0800

    updated dropdown menu with user blog, follows links

--- react-app/src/components/Header/NavBar/NavBar.js
+++ react-app/src/components/Header/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../../auth/LogoutButton';
 import './NavBar.css'
@@ -8,12 +8,24 @@ import SignUpFormModal from '../../auth/SignUp/SignUpModal';
 import CreateFormModal from '../../CreatePost/CreatePostModal';
 import { useState, useEffect } from 'react';
 
+import * as followActions from '../../../store/follow'
+
 const NavBar = () => {
   const sessionUser = useSelector(state => state.session.user)
+  const follows = useSelector(state => state.follows)
+
+  const dispatch = useDispatch()
+
+  console.log('navbar follows variable: ', follows)
 
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => setShowMenu(!showMenu);
+
+  useEffect(() => {
+    dispatch(followActions.getAllFollowers());
+    dispatch(followActions.getAllFollowing());
+  }, [dispatch])
 
   useEffect(() => {
     if (!showMenu) return;
@@ -26,6 +38,9 @@ const NavBar = () => {
 
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
+
+  const numFollowers = Object.keys(follows.followers).length
+  const numFollowing = Object.keys(follows.following).length
 
   return (
     <div id='header-links'>
@@ -55,10 +70,29 @@ const NavBar = () => {
                     <LogoutButton />
                   </div>
                   <div>
-                    <NavLink to='/following' exact={true} activeClassName='active' className='dropdown-option'>
-                      <i class="fa-solid fa-user-plus" />
-                      <p>Following</p>
+                    <NavLink to={`/users/${sessionUser.id}`} exact={true} activeClassName='active' className='dropdown-option'>
+                      <i class="fa-solid fa-user-large" />
+                       <p>Your posts</p>
                     </NavLink>
+                    <NavLink to='/following' exact={true} activeClassName='active' className='dropdown-option dropdown-followlink'>
+                      <div className='dropdown-option-child'>
+                        <i class="fa-solid fa-user-plus" />
+                        <p>Following</p>
+                      </div>
+                      <div className='dropdown-option-child'>
+                      <p>{numFollowing}</p>
+                      </div>
+                    </NavLink>
+                    <NavLink to='/followers' exact={true} activeClassName='active' className='dropdown-option dropdown-followlink'>
+                      <div className='dropdown-option-child'>
+                        <i class="fa-solid fa-users"></i>
+                        <p>Followers</p>
+                      </div>
+                      <div className='dropdown-option-child'>
+                      <p>{numFollowers}</p>
+                      </div>
+                    </NavLink>
+
                   </div>
                 </div>
               </div>
